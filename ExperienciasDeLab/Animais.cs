45bd437d4d111934cdbfae8b47fa56843db54736
Rafael <rafa.viturino94@gmail.com>
2019-10-10 17:39:09 -0300

    Adicionando Novo Projeto de expe de lab

---
+++ ExperienciasDeLab/Animais.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExperienciasDeLab
+{
+    class Animais
+    {
+        public int Sapo = 0;
+        public int Rato = 0;
+        public int Coelho = 0;
+        public int Total;
+        public double PorcentagemSapo;
+        public double PorcentagemRato;
+        public double PorcentagemCoelho;
+
+
+
+        public void Quantia(string animal, int quantidade)
+        {
+        if(animal == "S")
+            {
+                Sapo += quantidade;
+            }
+        else if (animal == "R")
+            {
+                Rato += quantidade;
+            }
+        else if (animal == "C")
+            {
+                Coelho += quantidade;
+            }
+        else
+            {
+                Console.WriteLine("Dados incorretos !");
+            }
+            Total = Sapo + Rato + Coelho;
+            PorcentagemSapo = (double) Sapo * 100 / Total;
+            PorcentagemRato = (double)Rato * 100 / Total;
+            PorcentagemCoelho = (double)Coelho * 100 / Total;
+        }
+        
+        public override string ToString()
+        {
+            return "Total: " + Total + "cobaias " +
+                    " .Total de coelhos: " +Coelho
+                   + " .Total de ratos: " + Rato +
+                   " .Total de sapos: " + Sapo +
+                   " .Percentual de coelhos: " + PorcentagemCoelho.ToString("F2", CultureInfo.InvariantCulture) +" % " +
+                   " .Percentual de ratos: "+ PorcentagemRato.ToString("F2", CultureInfo.InvariantCulture) + " % " +
+                   " .Percentual de sapos: " + PorcentagemSapo.ToString("F2", CultureInfo.InvariantCulture) + " % ";
+        }
+    }
+}
---
+++ ExperienciasDeLab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExperienciasDeLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animais a;
+           
+            Console.WriteLine("Informe o nome de um animal e a quantidade que possui dele");
+            string[] vet = Console.ReadLine().Split(' ');
+            string animal = vet[0];
+            int quantidade =int.Parse(vet[1]);
+
+            a = new Animais();
+            
+
+            while (animal == "S" || animal == "C" || animal == "R")
+            {
+                a.Quantia(animal, quantidade);
+                vet = Console.ReadLine().Split(' ');
+                animal = vet[0];
+                quantidade = int.Parse(vet[1]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
